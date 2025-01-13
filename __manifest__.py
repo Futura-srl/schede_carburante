9cd4197a8d62fe1ef54b53b5e62cd1d85b559f7e
Luca-Cocozza <luca.cocozza@futurasl.com>
2025-01-13 14:19:41 +0100

    Update to 17

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Schede carburante',
-    'version': '16',
+    'version': '17',
     'author': "Luca Cocozza",
     'application': True,
     'description': "Gestione delle schede carburante.",
